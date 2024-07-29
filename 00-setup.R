41e8226c8824ba88439ab971cc5304ce8592b864
juanrocha <aguilajk@gmail.com>
2024-07-29 22:42:46 +0200

    delete unnecessary libraries

--- 00-setup.R
+++ 00-setup.R
@@ -1,1 +1,14 @@
-rmarkdown::default_site_generator()
+library(rmarkdown)
+library(tictoc)
+## Create a website template
+#rmarkdown::default_site_generator()
+
+## Serve all pages
+tic()
+rmarkdown::render_site()
+toc() # 225.253 sec (3.75min) elapsed, basic website with 31 generic regime shifts
+
+
+tic()
+rmarkdown::clean_site()
+toc()
